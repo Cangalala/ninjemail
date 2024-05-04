2e44d4f69465ae54301030d3bc631fc43ae770ac
Cangalala <103964892+Cangalala@users.noreply.github.com>
2024-05-03 22:32:05 -0400

    Update __init__.py

--- ninjemail/__init__.py
+++ ninjemail/__init__.py
@@ -1,1 +1,19 @@
-from .ninjemail_manager import Ninjemail
+from ninjemail import Ninjemail
+
+# Replace "YOUR_API_KEY", "USERNAME" and "TOKEN" with your actual keys
+ninja = Ninjemail(
+    		browser="undetected-chrome",
+    		captcha_keys={"capsolver": "YOUR_API_KEY"},
+    		sms_keys={"getsmscode": {"user": "USERNAME", "token": "TOKEN"}},
+			auto_proxy=True)
+email, password = ninja.create_outlook_account(
+    					username="testuser", 
+    					password="testpassword", 
+    					first_name="John", 
+    					last_name="Doe", 
+    					country="USA", 
+    					birthdate="01-01-1990"
+)
+
+print(f"Email: {email}")
+print(f"Password: {password}")
